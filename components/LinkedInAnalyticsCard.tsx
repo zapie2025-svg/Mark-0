15d9bb98a7a40fcc3e397c742c2dd61c7e05a464
zapie2025-svg <zapie2025@gmail.com>
2025-08-29 00:10:41 +0530

    Fix LinkedIn OAuth: Use environment variable for client ID instead of placeholder

--- components/LinkedInAnalyticsCard.tsx
+++ components/LinkedInAnalyticsCard.tsx
@@ -62,7 +62,7 @@ export default function LinkedInAnalyticsCard() {
       const popup = window.open(
         'https://www.linkedin.com/oauth/v2/authorization?' +
         'response_type=code' +
-        '&client_id=YOUR_LINKEDIN_CLIENT_ID' + // You'll need to replace this
+        '&client_id=' + process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_ID +
         '&redirect_uri=' + encodeURIComponent(`${window.location.origin}/linkedin-callback`) +
         '&scope=' + encodeURIComponent('openid profile email w_member_social') +
         '&state=' + Math.random().toString(36).substring(7),
